f50ecfb51d30074d2068aebb2208035d2667f49b
ricof30 <97009967+ricof30@users.noreply.github.com>
2023-12-26 13:49:16 +0800

    Add files via upload

---
+++ lavalust.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 17, 2023 at 12:09 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lavalust`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `verification_code` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `is_verified` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=166 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `verification_code`, `is_verified`) VALUES
+(157, 'rico', 'fontecillaricof@gmail.com', '12345678', 'e90bc3ab', 1),
+(161, 'rico', 'fontecillaricof@gmail.com', '12345678', 'e90bc3ab', 0),
+(160, 'rico', 'ricofontecilla30@gmail.com', '12345678', '7bc78d34', 0),
+(162, 'rico', 'ricofontecilla30@gmail.com', '3eeafe00', '7cef8c77', 0),
+(163, 'rico', 'eramae02@gmail.com', '3eeafe00', '3ccae3eb', 1),
+(164, 'ricof', 'fontecillaricof@gmail.com', '12345678', 'e90bc3ab', 0),
+(165, 'rico', 'fontecillaricof@gmail.com', '12345678', 'e90bc3ab', 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
